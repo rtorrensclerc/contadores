9afe9422ff55655c665dee56c417ca39d2c2bd60
rtorrensclerc <rtorrens@dimacofi.cl>
2020-04-02 18:46:54 -0300

    Updates

---
+++ precache-manifest.7c9b9b1ab5c77cd559a5c3a95212a8d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7405ec2f23b5b35187d45a5d976b4c3a",
+    "url": "/contadores/index.html"
+  },
+  {
+    "revision": "c6ec02c4763111368f2c",
+    "url": "/contadores/static/css/main.dd547555.chunk.css"
+  },
+  {
+    "revision": "4064ed2d85e86d56042f",
+    "url": "/contadores/static/js/2.100d44cc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/contadores/static/js/2.100d44cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6ec02c4763111368f2c",
+    "url": "/contadores/static/js/main.ac7c8557.chunk.js"
+  },
+  {
+    "revision": "37e849876b9defcfa2e6",
+    "url": "/contadores/static/js/runtime-main.7137c4ff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/contadores/static/media/logo.5d5d9eef.svg"
+  }
+]);
